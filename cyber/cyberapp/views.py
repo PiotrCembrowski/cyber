d5dc5cd775825007b6e170befb6cce011486f9e5
PiotrCembrowski <losttape@gmail.com>
2025-10-10 16:28:43 +0200

    htmls index and tasks created

--- cyber/cyberapp/views.py
+++ cyber/cyberapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'cyber/index.html')
+
+def tasks(request):
+    return render(request, 'cyber/tasks.html')

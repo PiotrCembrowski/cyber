a7b3673b4d7ed24984ead2708794960c6ead6190
PiotrCembrowski <losttape@gmail.com>
2025-10-10 13:29:10 +0200

    warrior class

--- cyber/cyberapp/models.py
+++ cyber/cyberapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class WarriorClass(models.Model):
+    name = models.CharField(max_length=64)
+    profession = models.CharField(max_length=64)
+    level = models.IntegerField(default=1)
+    health = models.IntegerField()
+    strength = models.IntegerField()
+    intelligence = models.IntegerField()
+    dexterity = models.IntegerField()
+    condition = models.IntegerField()
+    move1 = models.CharField(max_length=64)
+    move2 = models.CharField(max_length=64)
+    move3 = models.CharField(max_length=64)
+    move4 = models.CharField(max_length=64)
